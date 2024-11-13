9e9450d3546dcaeb162e445e68c083f57cdbfd6e
CodingWithJamal <43446907+jamallyons@users.noreply.github.com>
2024-11-13 14:38:01 -0500

    fix build errors

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,18 +1,13 @@
-// use convex::ConvexClient;
+use convex::ConvexClient;
 
-// const CONVEX_URL: &str = "https://notable-orca-705.convex.cloud";
+const CONVEX_URL: &str = "https://notable-orca-705.convex.cloud";
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>>
-// {
-//     // let client = ConvexClient::new(CONVEX_URL).await?;
-
-//     println!("Hello, world!");
-
-//     Ok(())
-// }
-
-fn main()
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>
 {
+    // let client = ConvexClient::new(CONVEX_URL).await?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
